6cf762114dfe2627dbf321b09d1beecbc106a925
ainifadillahn <ainifadillahn@gmail.com>
2025-12-24 11:17:10 +0700

    Fix MLflow model logging for Docker build

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,12 +4,21 @@ from sklearn.metrics import roc_auc_score
 import mlflow
 import mlflow.sklearn
 import argparse
+import os
 
+# ============================
+# MLflow CONFIG
+# ============================
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("ai4i2020-experiment")
 mlflow.autolog()
 
 def main(data_path):
-    train_df = pd.read_csv(f"{data_path}/train.csv")
-    test_df  = pd.read_csv(f"{data_path}/test.csv")
+    # ============================
+    # Load data
+    # ============================
+    train_df = pd.read_csv(os.path.join(data_path, "train.csv"))
+    test_df  = pd.read_csv(os.path.join(data_path, "test.csv"))
 
     X_train = train_df.drop(columns=["Machine failure"])
     y_train = train_df["Machine failure"]
@@ -17,6 +26,9 @@ def main(data_path):
     X_test = test_df.drop(columns=["Machine failure"])
     y_test = test_df["Machine failure"]
 
+    # ============================
+    # Train model
+    # ============================
     model = RandomForestClassifier(
         n_estimators=100,
         random_state=42
@@ -24,15 +36,21 @@ def main(data_path):
 
     model.fit(X_train, y_train)
 
+    # ============================
+    # Log model (TANPA registry)
+    # ============================
     mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model",
-    registered_model_name="MachineFailureRF"
+        sk_model=model,
+        name="model"
     )
 
+    # ============================
+    # Evaluation
+    # ============================
     y_proba = model.predict_proba(X_test)[:, 1]
     auc = roc_auc_score(y_test, y_proba)
 
+    mlflow.log_metric("roc_auc", auc)
     print("ROC AUC:", auc)
 
 if __name__ == "__main__":
@@ -40,7 +58,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--data_path",
         type=str,
-        default="ai4i2020_preprocessed" 
+        default="ai4i2020_preprocessed"
     )
     args = parser.parse_args()
 
